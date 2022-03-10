4e370f0d5930edd93e8bd5946746c667f3b25e7a
Rehan Shekh <rehanrshekh@gmail.com>
2022-03-11 01:16:13 +0530

    RF2

--- src/com/blz/UseCase.java
+++ src/com/blz/UseCase.java
@@ -1,17 +1,27 @@
 package com.blz;
 
-public class UseCase {
+public class UseCase<T extends Comparable<T>> {
+    T x;
+    T y;
+    T z;
+
+    public UseCase(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public static void main(String[] args) {
 
         Integer xint = 65, yint = 85, zint = 22;
-        UseCase.compute(xint, yint, zint);
+        new UseCase(xint, yint, zint).testmaximum();
         Float xf = 9.7f, yf = 17.66f, zf = 55.5f;
-        UseCase.compute(xf, yf, zf);
+        new UseCase(xf, yf, zf).testmaximum();
         String xs = "Pear", ys = "Apple", zs = "Orange";
-        UseCase.compute(xs, ys, zs);
+        new UseCase(xs, ys, zs).testmaximum();
     }
 
-    public static <T extends Comparable<T>> void compute(T x, T y, T z) {
+    public static <T extends Comparable<T>> T testmaximum(T x, T y, T z) {
         T max = x;
         if (y.compareTo(max) > 0) {
             max = y;
@@ -20,5 +30,10 @@ public class UseCase {
             max = z;
         }
         System.out.println(max + " is maximum");
+        return null;
+    }
+
+    public T testmaximum() {
+        return UseCase.testmaximum(x, y, z);
     }
 }
