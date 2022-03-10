6470ed15cac42221880a14edfe9755c96055227f
Rehan Shekh <rehanrshekh@gmail.com>
2022-03-11 00:57:10 +0530

    Refactor 1 Done

--- src/com/blz/UseCase.java
+++ src/com/blz/UseCase.java
@@ -3,34 +3,22 @@ package com.blz;
 public class UseCase {
     public static void main(String[] args) {
 
-        // Integer xint = 65, yint = 85, zint = 22;
-//        Float xf = 9.7f, yf=17.66f, zf = 55.5f;
+        Integer xint = 65, yint = 85, zint = 22;
+        UseCase.compute(xint, yint, zint);
+        Float xf = 9.7f, yf = 17.66f, zf = 55.5f;
+        UseCase.compute(xf, yf, zf);
         String xs = "Pear", ys = "Apple", zs = "Orange";
-//        int max = xint;
-//        if (yint.compareTo(max) > 0) {
-//            max = yint;
-//        }
-//        if (zint.compareTo(max) > 0) {
-//            max = zint;
-//        }
-//        System.out.println(max + " is max number");
-//       float maxt = xf;
-//        if (yf.compareTo(maxt) > 0) {
-//            maxt = yf;
-//        }
-//        if (zf.compareTo(maxt) > 0) {
-//            maxt = zf;
-//        }
-//        System.out.println(maxt + " is max number");
+        UseCase.compute(xs, ys, zs);
+    }
 
-        if ((ys.compareTo(xs) > 0) && (ys.compareTo(zs) > 0)) {
-            System.out.println(ys + " is max String");
+    public static <T extends Comparable<T>> void compute(T x, T y, T z) {
+        T max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
         }
-        if ((zs.compareTo(xs) > 0) && (zs.compareTo(ys) > 0)) {
-            System.out.println(zs + " is max String");
+        if (z.compareTo(max) > 0) {
+            max = z;
         }
-        if ((xs.compareTo(ys) > 0) && (xs.compareTo(zs) > 0)) {
-            System.out.println(xs + " is max String");
-        }
+        System.out.println(max + " is maximum");
     }
 }
