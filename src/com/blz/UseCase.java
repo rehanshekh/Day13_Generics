2d72192aa20f5e5f717371afff8096f3e02bd667
Rehan Shekh <rehanrshekh@gmail.com>
2022-03-11 16:48:03 +0530

    UC5 Done

--- src/com/blz/UseCase.java
+++ src/com/blz/UseCase.java
@@ -1,39 +1,41 @@
 package com.blz;
 
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 public class UseCase<T extends Comparable<T>> {
     T x;
     T y;
     T z;
+    T r;
 
-    public UseCase(T x, T y, T z) {
+    public UseCase(T x, T y, T z, T r) {
         this.x = x;
         this.y = y;
         this.z = z;
+        this.r = r;
     }
 
     public static void main(String[] args) {
 
-        Integer xint = 65, yint = 85, zint = 22;
-        new UseCase(xint, yint, zint).testmaximum();
-        Float xf = 9.7f, yf = 17.66f, zf = 55.5f;
-        new UseCase(xf, yf, zf).testmaximum();
-        String xs = "Pear", ys = "Apple", zs = "Orange";
-        new UseCase(xs, ys, zs).testmaximum();
+        Integer xint = 65, yint = 85, zint = 22, rint = 99;
+        new UseCase(xint, yint, zint, rint).testmaximum();
+        Float xf = 9.7f, yf = 17.66f, zf = 55.5f, rf = 69.4f;
+        new UseCase(xf, yf, zf, rf).testmaximum();
+        String xs = "Pear", ys = "Apple", zs = "Orange", rs = "Strawberry";
+        new UseCase(xs, ys, zs, rs).testmaximum();
     }
 
-    public static <T extends Comparable<T>> T testmaximum(T x, T y, T z) {
-        T max = x;
-        if (y.compareTo(max) > 0) {
-            max = y;
-        }
-        if (z.compareTo(max) > 0) {
-            max = z;
-        }
-        System.out.println(max + " is maximum");
+
+    public static <T extends Comparable<T>> @Nullable T testmaximum(T... a) {
+        Arrays.sort(a, Collections.reverseOrder());
+        System.out.println(a[0] + " is maximum");
         return null;
     }
 
     public T testmaximum() {
-        return UseCase.testmaximum(x, y, z);
+        return UseCase.testmaximum(x, y, z, r);
     }
 }
